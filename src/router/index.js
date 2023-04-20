3a5f1a775c2878e37dbca6294ee5adff0ae9e58a
liuche <706716852@qq.com>
2023-04-20 22:11:51 +0800

    2-1 实现了数据字典的功能模块

--- src/router/index.js
+++ src/router/index.js
@@ -55,36 +55,52 @@ export const constantRoutes = [
     }]
   },
 
-    // 医院设置管理
-    {
-      path: '/yygh/hospset',
-      component: Layout,
-      redirect: '/yygh/hospset/list',
-      name: 'Teacher',
-      meta: { title: '医院管理',icon: 'el-icon-s-help'  },
-      children: [
-        {
-          path: 'list',
-          name: 'HospSetList',
-          component: () => import('@/views/yygh/hospset/list'),
-          meta: { title: '医院设置列表',icon: 'table'  }
-        },
-        {
-          path: 'create',
-          name: 'HospSetCreate',
-          component: () => import('@/views/yygh/hospset/form'),
-          meta: { title: '添加医院设置',icon: 'tree'  }
-        },
-        {
-          path: 'edit/:id',
-          name: 'HospSetEdit',
-          component: () => import('@/views/yygh/hospset/form'),
-          meta: { title: '编辑医院设置', noCache: true },
-          hidden: true
-        }
-      ]
-    },
-  
+  // 医院设置管理
+  {
+    path: '/yygh/hospset',
+    component: Layout,
+    redirect: '/yygh/hospset/list',
+    name: 'Teacher',
+    meta: { title: '医院管理', icon: 'el-icon-s-help' },
+    children: [
+      {
+        path: 'list',
+        name: 'HospSetList',
+        component: () => import('@/views/yygh/hospset/list'),
+        meta: { title: '医院设置列表', icon: 'table' }
+      },
+      {
+        path: 'create',
+        name: 'HospSetCreate',
+        component: () => import('@/views/yygh/hospset/form'),
+        meta: { title: '添加医院设置', icon: 'tree' }
+      },
+      {
+        path: 'edit/:id',
+        name: 'HospSetEdit',
+        component: () => import('@/views/yygh/hospset/form'),
+        meta: { title: '编辑医院设置', noCache: true },
+        hidden: true
+      }
+    ]
+  },
+  // 数据字典
+  {
+    path: '/cmn',
+    component: Layout,
+    redirect: '/cmn/list',
+    name: '数据管理',
+    alwaysShow: true,
+    meta: { title: '数据管理', icon: 'example' },
+    children: [
+      {
+        path: 'list',
+        name: '数据字典',
+        component: () => import('@/views/dict/list'),
+        meta: { title: '数据字典', icon: 'table' }
+      }
+    ]
+  },
 
   {
     path: '/example',
