1d5548d1ac131fca48286de0fe036a8da58c729f
liuche <706716852@qq.com>
2023-04-21 17:08:02 +0800

    2-2 实现了excel的下载和写入功能

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -17,6 +17,35 @@
 
       <el-table-column prop="createTime" label="创建时间"></el-table-column>
     </el-table>
+    <div class="el-toolbar">
+      <div class="el-toolbar-body" style="justify-content: flex-start;">
+        <el-button type="text" @click="exportData">
+          <i class="fa fa-plus" /> 导出
+        </el-button>
+        <el-button type="text" @click="importData">
+          <i class="fa fa-plus" /> 导入
+        </el-button>
+      </div>
+    </div>
+    <!-- 弹出层 -->
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+      <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+          <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:9000/admin/cmn/dict/importData'"
+            class="upload-demo"
+          >
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">取消</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -25,6 +54,7 @@ import dict from "@/api/yygh/dict";
 export default {
   data() {
     return {
+      dialogImportVisible: false,
       list: [] //数据字典列表数组
     };
   },
@@ -42,6 +72,18 @@ export default {
       dict.dictList(tree.id).then(response => {
         resolve(response.data.list);
       });
+    },
+    exportData() {
+      window.open("http://localhost:9000/admin/cmn/dict/exportData");
+    },
+    importData() {
+      this.dialogImportVisible = true;
+    },
+
+    onUploadSuccess(response, file) {
+      this.$message.info("上传成功");
+      this.dialogImportVisible = false;
+      this.getDictList(1);
     }
   }
 };
