d115b2a95c45abfee417092b4f567152721f20d1
liuche <706716852@qq.com>
2023-04-30 18:00:08 +0800

    3-2 实现了用户列表的分页条件查询的功能

--- src/router/index.js
+++ src/router/index.js
@@ -125,6 +125,23 @@ export const constantRoutes = [
     meta: { title: '排班', noCache: true },
     hidden: true
   },
+  // 用户管理模块
+  {
+    path: '/user',
+        component: Layout,
+            redirect: '/user/userInfo/list',
+                name: 'userInfo',
+                    meta: { title: '用户管理', icon: 'table' },
+                        alwaysShow: true,
+                            children: [
+                                {
+                                    path: 'userInfo/list',
+                                    name: '用户列表',
+                                    component: () =>import('@/views/yygh/userInfo/list'),
+                                    meta: { title: '用户列表', icon: 'table' }
+                                }
+                            ]
+},
   {
     path: '/example',
     component: Layout,
